2443f8da663a4e63cac4a7f782a06f56a0a257fa
Tamal1 <tamalkrishna2026@gmail.com>
2023-10-09 16:43:40 +0600

    Finished

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -19,6 +19,7 @@ const Services = () => {
       </div>
    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
     {
+      
         services?.map(service =><ServiceCard key={service.id} service={service}></ServiceCard>)
     }
   
